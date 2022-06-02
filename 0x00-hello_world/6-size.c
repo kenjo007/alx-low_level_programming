358723149b7701e62d7d60abce75a2eb1060551a
kenjo007 <njogukm@gmail.com>
2022-06-02 13:46:28 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,14 +8,22 @@
 int main(void)
 {
 	char acharacter
+	               ^
+		       ;
 	int aintegrer
+		     ^
+		     ;
 	long along
+		  ^
+		  ;
 	long long alonglong
+		           ^
+			   ;
 	float afloat
-printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+__attribute__ printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+__attribute__ printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+__attribute__ printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+__attribute__ printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+__attribute__ printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 return (0);
 }
