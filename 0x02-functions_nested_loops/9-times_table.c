019420b572cb78187998d5028e0dc2c99c0ad2f8
kenjo007 <njogukm@gmail.com>
2022-06-08 14:38:11 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,11 +2,11 @@
 
 /**
  * *main - check the code
- *  *
- *   *Return: Always 0
- *    */
+ * *
+ * *Return: Always 0
+ * */
 int main(void)
 {
 	times_table();
-	return (0);
+            return (0);
 }
