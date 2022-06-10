7dc09106c618abee280ac0cbd5eeb82f02cb5445
kenjo007 <njogukm@gmail.com>
2022-06-10 12:16:41 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,14 @@ int main(void)
 	printf('infinite loop incoming :(\n");
 
 	i = 0;
-
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-
-	printf("infinite loop avoided! \\o/\n");
+	/**
+	*while (i < 10)*
+	*{
+	*putchar(i);
+	*}
+	*
+	* */
+	printf("infinite loop avoided! ||0/\n");
 
 	return (0);
 }
