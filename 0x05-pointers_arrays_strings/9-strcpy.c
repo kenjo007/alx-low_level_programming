9dd31c92871aa4a6fab89030a7525c1be4746b52
kenjo007 <njogukm@gmail.com>
2022-06-14 14:02:23 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _strcpy - update value
+ * @dest: value to be evaluate
+ * @src: value to be evaluate
+ * Return: not
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int 1 = 0;
+	int x = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < 1 ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
