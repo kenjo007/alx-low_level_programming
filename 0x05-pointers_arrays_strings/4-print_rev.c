3b3090b5d03d965ed129b3ec330ec06945faa7f9
kenjo007 <njogukm@gmail.com>
2022-06-14 13:11:31 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@ void print_rev(char *s)
 {
 	int len;
 
-	fir (len = 0; s[len] != '\0'; len++)
+	for (len = 0; s[len] != '\0'; len++)
 	{
 	}
-	for (len = len -1; len >= 0; len--)
+	for (len = len - 1; len >= 0; len--)
 	{
 		_putchar(s[len]);
 	}
